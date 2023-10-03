8ca2aa06ac0c609d1afde2d6fe37502d1d0e9d35
Mary Po <pnmry@yandex.ru>
2023-10-03 18:59:23 +0300

    1 more function added

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 import requests
 
 class BasePage():
@@ -14,3 +16,10 @@ class BasePage():
         code = 200
         status_code = requests.get(self.browser.current_url).status_code
         assert status_code == code, "Status not correct"
+        
+    def has_disappeared(self, how, what, timeout=10):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+        return True
