c2b404fe05ed18e9f590995aa7526e4e0cfcd78c
Mary Po <pnmry@yandex.ru>
2023-10-03 19:01:40 +0300

    more functions added

--- pages/result_page.py
+++ pages/result_page.py
@@ -3,6 +3,7 @@ from .search_page import SearchPage
 from .locators import ResultPageLocators
 from selenium.webdriver.common.by import By
 import requests
+import time
 
 class ResultPage(BasePage):
     def should_be_search_request_in_search_string(self, search_request):
@@ -23,3 +24,26 @@ class ResultPage(BasePage):
     def should_open_lens_page(self):
         lens_url = "https://lens.google.com/"
         assert lens_url in self.url, "Image search did not start"
+        
+    def should_click_cat_paw(self):
+        paw = self.browser.find_element(*ResultPageLocators.CAT_PAW)
+        paw.click()
+        paw_screen = self.browser.find_element(*ResultPageLocators.PAW_SCREEN)
+        paw_screen.click()
+        time.sleep(2)
+        paw_screen.click()
+        time.sleep(2)
+        paw_screen.click()
+        time.sleep(2)
+        assert paw_screen, "No cat - no paws :((("
+        
+    def should_close_cat_paw(self):
+        close_paw_button = self.browser.find_element(*ResultPageLocators.CLOSE_PAW_BUTTON)
+        close_paw_button.click()
+        self.has_disappeared(*ResultPageLocators.PAW_SCREEN)
+        paw = self.browser.find_element(*ResultPageLocators.CAT_PAW)
+        assert paw, "Cat does not want to leave :((("
+            
+    
+  
+        
